160d9a0ebedb895623cecdd6eb8730d4cc83e243
tssqzr <1214661466@qq.com>
2017-06-30 11:54:09 +0800

    update struct and update doc
    1. add SDK version info
    2. split assets operation steps
    <1> contruct transactions
    <2> signature transactions
    <3> send transaction
    
    Signed-off-by: tssqzr <1214661466@qq.com>

---
+++ src/main/java/DNA/IVersion.java
@@ -0,0 +1,13 @@
+package DNA;
+
+public class IVersion {
+	private static final String version = "v0.6.1";
+	public static final String[] versionInfo = new String[] {
+		"project:" + "DNA SDK",
+		"version:" + version,
+		"description:"+ "This version is suitable for DNA master when the newly tag of DNA  is 0.6alpha,  named sdk-v0.6.1",
+		"lastModified:"+"2017-06-26",
+		"author:"+"ts"
+	};
+
+}
